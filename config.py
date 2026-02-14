68a5f973b5a612754f236a6d9ff870c18ee2e122
Sahar Sistani <saharsistani137777@gmail.com>
2026-02-14 15:12:09 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,12 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+class Config:
+    TMDB_API_KEY = os.getenv('TMDB_API_KEY', 'your_tmdb_api_key_here')
+    DATABASE_PATH = 'movies.db'
+    SECRET_KEY = os.getenv('SECRET_KEY', 'dev-key-2026')
+    DEBUG = True
+    PORT = 5000
+    HOST = '0.0.0.0'
